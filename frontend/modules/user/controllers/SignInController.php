72380a3705becc8fcbe7428b5edafa5c3b6c4a9a
dev <roof1rst@gmail.com>
2015-05-26 15:28:45 +0300

    name

--- frontend/modules/user/controllers/SignInController.php
+++ frontend/modules/user/controllers/SignInController.php
@@ -74,7 +74,6 @@ class SignInController extends \yii\web\Controller
             return ActiveForm::validate($model);
         }
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            sleep(1); // button is so cute
             return $this->goBack();
         } else {
             return $this->renderAjax('login', [
