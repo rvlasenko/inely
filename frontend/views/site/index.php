67f96d1af61e28b86c1b62ee9c5894b3bc0d45e9
roman <admiralexo@gmail.com>
2015-06-20 09:48:17 +0300

    todo

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -84,90 +84,53 @@
                          style="height: 400px;">
                         <div class="mCustomScrollBox mCS-light" id="mCSB_31"
                              style="position:relative; height:100%; overflow:hidden; max-width:100%;">
-                            <div class="mCSB_container" style="position: relative; top: 0px;">
+                            <div class="mCSB_container" style="position: relative; top: 0;">
                                 <div class="panel-content">
                                     <ul class="todo-list ui-sortable">
                                         <li class="high">
-                      <span class="span-check">
-                      <input id="task-1" type="checkbox" data-checkbox="icheckbox_square-blue" />
-                      <label for="task-1"></label>
-                      </span>
+                                            <span class="span-check">
+                                                <input id="task-1" type="checkbox" data-checkbox="icheckbox_square-blue">
+                                                <label for="task-1"></label>
+                                            </span>
                                             <span class="todo-task">Send email to Bob Linch</span>
                                             <div class="todo-date clearfix">
                                                 <div class="completed-date"></div>
-                                                <div class="due-date">Due on <span class="due-date-span">15 December 2014</span></div>
+                                                <div class="due-date">До
+                                                    <span class="due-date-span">15 декабря</span>
+                                                </div>
                                             </div>
-                      <span class="todo-options pull-right">
-                      <a href="javascript:;" class="todo-delete"><i class="icons-office-52"></i></a>
-                      </span>
+                                            <span class="todo-options pull-right">
+                                                <a href="#" class="todo-delete">
+                                                    <i class="fa fa-times"></i>
+                                                </a>
+                                            </span>
                                             <div class="todo-tags pull-right">
                                                 <div class="label label-success">Work</div>
                                             </div>
                                         </li>
-                                        <li>
-                      <span class="span-check">
-                      <input id="task-2" type="checkbox" data-checkbox="icheckbox_square-blue"/>
-                      <label for="task-2"></label>
-                      </span>
-                                            <span class="todo-task">Call datacenter for servers</span>
+                                        <li class="high">
+                                            <span class="span-check">
+                                                <input id="task-1" type="checkbox" data-checkbox="icheckbox_square-blue">
+                                                <label for="task-1"></label>
+                                            </span>
+                                            <span class="todo-task">Send email to Bob Linch</span>
                                             <div class="todo-date clearfix">
                                                 <div class="completed-date"></div>
-                                                <div class="due-date">Due on <span class="due-date-span">7 January</span></div>
+                                                <div class="due-date">До
+                                                    <span class="due-date-span">15 декабря</span>
+                                                </div>
                                             </div>
-                      <span class="todo-options pull-right">
-                      <a href="javascript:;" class="todo-delete"><i class="icons-office-52"></i></a>
-                      </span>
-                                        </li>
-                                        <li class="low">
-                      <span class="span-check">
-                      <input id="task-3" type="checkbox" data-checkbox="icheckbox_square-blue"/>
-                      <label for="task-3"></label>
-                      </span>
-                                            <span class="todo-task">Remove all unused icons</span>
-                                            <div class="todo-date clearfix">
-                                                <div class="completed-date"></div>
-                                                <div class="due-date">Due on <span class="due-date-span">5 January</span></div>
-                                            </div>
-                      <span class="todo-options pull-right">
-                      <a href="javascript:;" class="todo-delete"><i class="icons-office-52"></i></a>
-                      </span>
-                                        </li>
-                                        <li class="medium">
-                      <span class="span-check">
-                      <input id="task-4" type="checkbox" data-checkbox="icheckbox_square-blue"/>
-                      <label for="task-4"></label>
-                      </span>
-                                            <span class="todo-task">Read my todo list</span>
-                                            <div class="todo-date clearfix">
-                                                <div class="completed-date"></div>
-                                                <div class="due-date">Due on <span class="due-date-span">4 January</span></div>
-                                            </div>
-                      <span class="todo-options pull-right">
-                      <a href="javascript:;" class="todo-delete"><i class="icons-office-52"></i></a>
-                      </span>
+                                            <span class="todo-options pull-right">
+                                                <a href="#" class="todo-delete">
+                                                    <i class="fa fa-times"></i>
+                                                </a>
+                                            </span>
                                             <div class="todo-tags pull-right">
-                                                <div class="label label-info">Tuesday</div>
+                                                <div class="label label-success">Work</div>
                                             </div>
-                                        </li>
-                                        <li>
-                      <span class="span-check">
-                      <input id="task-6" type="checkbox" data-checkbox="icheckbox_square-blue"/>
-                      <label for="task-6"></label>
-                      </span>
-                                            <span class="todo-task">Have a breakfeast before 12</span>
-                                            <div class="todo-date clearfix">
-                                                <div class="completed-date"></div>
-                                                <div class="due-date">Due on <span class="due-date-span">1 January</span></div>
-                                            </div>
-                      <span class="todo-options pull-right">
-                      <a href="javascript:;" class="todo-delete"><i class="icons-office-52"></i></a>
-                      </span>
                                         </li>
                                     </ul>
                                     <div class="clearfix m-t-10">
-                                        <div class="pull-left">
-                                            <button type="button" class="btn btn-sm btn-default check-all-tasks">Check All Done</button>
-                                        </div>
                                         <div class="pull-right">
                                             <button type="button" class="btn btn-sm btn-dark add-task">Add Task</button>
                                         </div>
