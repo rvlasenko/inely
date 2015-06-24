621be61f8c99da92267d30bb376b857b17344ab1
roman <admiralexo@gmail.com>
2015-06-24 20:16:25 +0300

    cleaned

--- frontend/views/layouts/_error.php
+++ frontend/views/layouts/_error.php
@@ -25,14 +25,6 @@
 
     <?= $content ?>
 
-<div class="loader-overlay">
-    <div class="spinner">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>
-    </div>
-</div>
-
 <?php $this->endBody() ?>
 </body>
 </html>
