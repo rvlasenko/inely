b77de1e2a38308284efaabc92f702808e8fc7bfe
roman <admiralexo@gmail.com>
2015-07-20 14:27:48 +0300

    refactor

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -10,13 +10,12 @@ require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 // Bootstrap application
 require(__DIR__ . '/../../common/config/bootstrap.php');
-require(__DIR__ . '/../config/bootstrap.php');
 
 $config = \yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/base.php'),
-    require(__DIR__ . '/../../common/config/web.php'),
+    require(__DIR__ . '/../../common/config/main.php'),
     require(__DIR__ . '/../config/base.php'),
-    require(__DIR__ . '/../config/web.php')
+    require(__DIR__ . '/../config/main.php')
 );
 
 (new yii\web\Application($config))->run();
